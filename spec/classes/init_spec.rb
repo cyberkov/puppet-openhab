32f558f304730da816ab8dd161bd87168493a881
Hannes Schaller <admin@cyberkov.at>
2015-07-27 16:00:34 +0200

    cover all resources

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -11,6 +11,17 @@ describe 'openhab' do
     let(:facts) { {:lsbdistid => 'Debian', :osfamily => 'Debian' } }
 
     it { should contain_apt__source('openhab') }
+    it { should contain_package('openhab-runtime') }
+    it { should contain_service('openhab') }
+    it { should contain_user('openhab') }
+  end
 
+  context 'with module array defined' do
+    let(:params) { {:modules => ['openhab-addon-action-homematic', 'openhab-addon-action-pushover'] } }
+
+    it { 
+      should contain_package('openhab-addon-action-homematic')
+      should contain_package('openhab-addon-action-pushover')
+    }
   end
 end
